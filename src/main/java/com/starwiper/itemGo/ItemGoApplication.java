75786e021d48837945231d4a771bf38268825534
AFei <tjtgcyf@gmail.com>
2024-11-22 22:43:10 +0900

    first add

---
+++ src/main/java/com/starwiper/itemGo/ItemGoApplication.java
@@ -0,0 +1,13 @@
+package com.starwiper.itemGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemGoApplication.class, args);
+	}
+
+}
