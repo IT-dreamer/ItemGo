55151e833bca2ada2b6b831e5cef104c19fa4ed4
AFei <tjtgcyf@gmail.com>
2024-11-22 22:44:05 +0900

    add: UT

---
+++ src/test/java/com/starwiper/itemGo/ItemGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starwiper.itemGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
